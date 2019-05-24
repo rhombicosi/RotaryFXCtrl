eceb93a9fd9ef10061e862a6196660e12d1955da
Olia <ok.00100@gmail.com>
2019-05-24 00:09:24 -0400

    MultiInterrupt example

---
+++ Rotary/Rotary.h
@@ -0,0 +1,46 @@
+/*
+ * Rotary encoder library for Arduino.
+ */
+
+#ifndef rotary_h
+#define rotary_h
+
+#include "Arduino.h"
+
+ // Enable this to emit codes twice per step.
+#define HALF_STEP
+
+ // Enable weak pullups
+#define ENABLE_PULLUPS
+
+// Values returned by 'process'
+// No complete step yet.
+#define DIR_NONE 0x0
+// Clockwise step.
+#define DIR_CW 0x10
+// Anti-clockwise step.
+#define DIR_CCW 0x20
+
+class Rotary
+{
+	public:
+		Rotary(char, char);
+		// Process pin(s)
+		unsigned char process();
+		void prnt();
+		void rotate();
+	private:
+		unsigned char state;
+		unsigned char pin1;
+		unsigned char pin2;
+
+		const int encoderStepsPerRevolution = 30;
+
+		volatile int counter;
+		volatile int angle;
+		volatile int period;
+
+		volatile unsigned char result;
+};
+
+#endif
---
+++ Rotary/examples/multiinterrupt/multiinterrupt/multiinterrupt.ino
@@ -0,0 +1,61 @@
+/*
+ * Example using the Rotary library, dumping integers to the serial
+ * port. The integers increment or decrement depending on the direction
+ * of rotation.
+ *
+ * This example uses interrupts rather than polling.
+ */
+
+#include <Rotary.h>
+
+const int CLK = 14;
+const int DT = 15;
+
+const int CLK0 = 32;
+const int DT0 = 33;
+
+const int encoderStepsPerRevolution = 30;
+volatile int angle = 0;
+volatile int period = 0;
+
+volatile int angle0 = 0;
+
+// Rotary encoder is wired with the common to ground and the two
+// outputs to pins 14 and 15.
+Rotary ro = Rotary(14, 15);
+Rotary ro0 = Rotary(32, 33);
+
+// Counter that will be incremented or decremented by rotation.
+int counter = 0;
+int counter0 = 0;
+
+void setup() {
+  Serial.begin(115200);
+  
+  pinMode(CLK, INPUT_PULLUP);
+  pinMode(DT, INPUT_PULLUP);
+
+  pinMode(CLK0, INPUT_PULLUP);
+  pinMode(DT0, INPUT_PULLUP);
+
+
+  attachInterrupt(digitalPinToInterrupt(CLK), rotor, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(DT), rotor, CHANGE);
+
+  attachInterrupt(digitalPinToInterrupt(CLK0), rotor0, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(DT0), rotor0, CHANGE);
+}
+
+void loop() {
+}
+
+void rotor() 
+{
+  ro.process();
+  ro.rotate();
+
+void rotor0()
+{
+  ro0.process();
+  ro0.rotate();
+}
