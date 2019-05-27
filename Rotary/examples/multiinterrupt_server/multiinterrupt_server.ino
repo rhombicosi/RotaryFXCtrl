2b7de66b8d479546484345dfef8b5c5bb1abd27e
Olia <ok.00100@gmail.com>
2019-05-26 23:50:04 -0400

    webserver

---
+++ Rotary/examples/multiinterrupt_server/multiinterrupt_server.ino
@@ -0,0 +1,95 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include <WebSocketsServer.h>
+#include <Ticker.h>
+
+#include <Rotary.h>
+
+/* server part */
+Ticker timer;
+
+char * ssid = "HEXECONTAHEDRON";
+char * password = "salamander";
+
+WebServer server;
+WebSocketsServer Socket = WebSocketsServer(81);
+
+char webpage[] PROGMEM =
+#include "webpage.h"
+;
+
+/* encoder part */
+const int CLK[2] = {14, 32};
+const int DT[2] = {15, 33};
+
+Rotary ro[2] = { Rotary(CLK[0], DT[0]), Rotary(CLK[1], DT[1])};
+
+void setup() { 
+
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+          Serial.print(".");
+          delay(500);
+  }
+  
+  Serial.println("");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/",[](){
+    server.send_P(200, "text/html", webpage);
+  });  
+  server.begin();
+  
+  Socket.begin();
+  Socket.onEvent(webSocketEvent);   
+
+  timer.attach(0.2, getData);
+  
+  for(int i = 0; i < 2; i++) {
+    pinMode(CLK[i], INPUT_PULLUP);
+    pinMode(DT[i], INPUT_PULLUP);
+  }
+  
+  attachInterrupt(digitalPinToInterrupt(CLK[0]), rotate0, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(DT[0]), rotate0, CHANGE);
+
+  attachInterrupt(digitalPinToInterrupt(CLK[1]), rotate1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(DT[1]), rotate1, CHANGE);
+}
+
+void loop() {
+  server.handleClient();
+  Socket.loop();
+}
+
+void rotate0() 
+{
+  ro[0].process();
+  ro[0].rotate();
+}
+
+void rotate1()
+{
+  ro[1].process();
+  ro[1].rotate();
+}
+
+void getData(){
+  String json = "{\"encoder\": [";
+  
+  for (int i = 0; i < 2; i++){
+    json += CLK[i];
+    if(i != 1) {
+      json += ",";  
+    }
+  }
+  
+  json += "]}";
+  Socket.broadcastTXT(json.c_str(), json.length());
+}
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length){
+}
---
+++ Rotary/examples/multiinterrupt_server/webpage.h
@@ -0,0 +1,46 @@
+R"=====(
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>RotaryFXCtrl</title>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.2/p5.min.js'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.2/addons/p5.dom.min.js'></script> 
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/smoothie/1.32.0/smoothie.min.js'></script> 
+  <style type="text/css">
+    body {
+      background: black;
+      color: white;
+      line-height: 100%;
+      font-size: 1rem;
+    }    
+    h1 {
+        font-size: 1em;
+        font-family: sans-serif;
+        color: gray;
+    }
+    .wrapper {
+        display: flex;
+        flex-flow: column wrap;
+        align-content: space-between;
+        -ms-flex-line-pack: justify;
+        height: 100%;
+    }
+    .wrapper .block {
+        width: 49%;
+        margin: 0 0 10px 0;
+    }
+    .plot {
+        margin: 0 0 10px 0;
+    }    
+    .geometry {
+        background-color: black;
+        width: 100% !important;
+        height: 55.55% !important;
+    }        
+  </style> 
+</head>
+<body>
+<p><h1>RotaryFXCtrl</h1></p>
+</body>
+</html>
+)====="
