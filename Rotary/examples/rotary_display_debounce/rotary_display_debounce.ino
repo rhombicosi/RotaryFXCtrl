06451fa87313964492ebc48c1a8d6f56e69310f4
Olia <ok.00100@gmail.com>
2019-07-15 03:16:20 -0400

    login procedure

--- Rotary/examples/rotary_display_debounce/rotary_display_debounce.ino
+++ Rotary/examples/rotary_display_debounce/rotary_display_debounce.ino
@@ -1,9 +1,7 @@
 #include <WiFi.h>
 #include <WebServer.h>
 #include <WebSocketsServer.h>
-
 #include <EEPROM.h>
-
 #include <Ticker.h>
 #include <Rotary.h>
 #include <SPI.h>
@@ -38,26 +36,32 @@ int b;
 int scan = 0; // number of scans performed
 int n = 0; // number of scanned networks
 int print_count = 0; // counts how many times some selected info is printed
-String ssid = ""; // ssid
-bool ssid_selected = 0; 
-
-String password = String();
-bool password_set = 0;
-
-String id = ""; 
-String oldid = "";
-
+String ssid = String(); // stores ssid
+bool ssid_selected = 0; // true if ssid is selected
+String password = String(); // stores password
+bool password_set = 0; // true if password set
+String id = ""; // ssid displayed
+String oldid = ""; // previos ssid displayed
 int letter;
 int oldletter;
+String pswd = "";
+String oldpswd = "";
 
 unsigned long timeout = 0;
 int readeep = 0; // EEPROM read?
 int start = 0; // server started?
 
+bool eep = false;
+
 void setup() 
 { 
   Serial.begin(115200);
   EEPROM.begin(512);
+
+//  for test
+//  Serial.println("clearing eeprom");
+//  for (int i = 0; i < 96; ++i) { EEPROM.write(i, 0); }
+//  EEPROM.commit();
   
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x32
   display.clearDisplay();
@@ -67,7 +71,7 @@ void setup()
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
-  display.println("rotary encoders V1.0.0.0");
+  display.println("rotary encoders V1");
   display.display();
 
   // wifi part   
@@ -90,51 +94,42 @@ void setup()
   attachInterrupt(digitalPinToInterrupt(DT[1]), rotate1, CHANGE);  
 }
 
+int creds_received = 0;
+
 void loop() 
 { 
+  b = checkButton();
+  
   if (!readeep)
   {
     read_eep();
     readeep = 1;
-  }  
-  
-  if (!start && readeep && ssid.length() > 1) 
-  {    
-    WiFi.begin(ssid.c_str(), password.c_str());      
-    display.clearDisplay();
-    display.setCursor(0,0);
-    timeout = millis();
-    while (WiFi.status()!= WL_CONNECTED && millis() - timeout < 30000 && !scan ) 
-    { 
-      display.print(">");   
-      display.display();    
-      delay(10);             
-    }
-    if (WiFi.status() == WL_CONNECTED) 
+
+    //if (ssid != "")
+    if (ssid.length() > 1)
     {
-      if (print_count == 0)
+      scan = 1;
+      Serial.println("SSID repeat " + ssid);
+      Serial.println("SSID length " + String(ssid.length()));
+      if (password.length() > 1)
       {
-        display.println(char(1));
-        display.println(WiFi.localIP());
-        display.display();
-        print_count++;
+        creds_received = 1;
+        eep = true;
       }
-      scan = 1;      
     }
-  }
-  
+    else
+    {
+      Serial.print("ssid is empty");
+    }
+  } 
   if (!scan) 
   {
-    b = checkButton();
-    
     Serial.println("scan start");
-    unsigned long t0 = millis();
-    unsigned long dif = millis() - t0;
-    n = WiFi.scanNetworks(); 
-    scan = 1;
-    dif = millis() - t0;
-    Serial.println("scan time: " +  String(dif));
-  
+    n = WiFi.scanNetworks();
+    scan = 1; 
+  }
+  else if (!creds_received)
+  {
     if (n == 0) 
     {
       Serial.println("no networks found");
@@ -150,11 +145,13 @@ void loop()
         {         
           Serial.print(i + 1);
           Serial.print(": "); 
-          Serial.println( WiFi.SSID(i));
+          Serial.println(WiFi.SSID(i));
         }
   
         Serial.println("select SSID: ");       
         Serial.println(" "); 
+  
+        // TODO show this info on dispay
         
         print_count++;  
       }
@@ -168,6 +165,7 @@ void loop()
           display.clearDisplay();    
           display.display();
           display.setCursor(0,0);
+  
           display.println(String(id));
           display.display();  
           oldid = id;
@@ -178,6 +176,7 @@ void loop()
         {
           ssid = id;
           ssid_selected = 1;
+          password = String();
           Serial.println("button 0 pressed");
           Serial.println("selected network: " + ssid);
         }
@@ -194,58 +193,76 @@ void loop()
           display.print(password);
           display.print(char(letter));
           display.display();
-  
+         
           Serial.println(char(letter));
           oldletter=letter;
         }
         
+  //      if (!password_set && oldpswd != password)
+  //      {
+  //        display.fillRect(0,8,128,8,BLACK);
+  //        display.display();
+  //        display.setCursor(0,8);
+  //        display.print(password);
+  //        oldpswd = password;
+  //      }
+  //
+  //      if (!password_set && oldletter != letter)
+  //      {          
+  //        display.print(char(letter));
+  //        display.display();
+  //
+  //        Serial.println(char(letter));
+  //        oldletter=letter;
+  //      }
+        
         if (b == 1 && !password_set)
-        {        
+        { 
           password = password + char(letter);  
           Serial.println(password);
         }
   
-        if (b == 3)
+        if (b == 4)
         {
           Serial.println("password set: " + password);
-          password_set = 1;        
-        }
-      }    
-  
-      if(password_set)
-      {
-        // login
-        WiFi.begin(ssid.c_str(), password.c_str());
-        display.setCursor(0,16);
-        while (WiFi.status() != WL_CONNECTED) 
-        {
-          delay(500);          
-          display.print(">");   
-          display.display();                 
-        }
-  
-        if (print_count == 1)
-        {
-          Serial.println("WiFi Connected.");
-          display.println(char(1));
-          display.display();
-          Serial.print("IP Address: ");
-          Serial.println(WiFi.localIP());
-          display.println(WiFi.localIP());
-          display.display();
-  
-          print_count++;
-        }
-  
-        if (WiFi.status() == WL_CONNECTED)
-        {
-          update_eep();
-        }
-       } 
-       delay(10);
-    }  
+          password_set = 1;  
+          creds_received = 1;      
+        }        
+      }
+    } 
   }
 
+  if (!start && creds_received)
+  {
+    // login
+    Serial.println(" creds " + String(creds_received));
+    
+    WiFi.begin(ssid.c_str(), password.c_str());
+    display.setCursor(0,16);
+    while (WiFi.status() != WL_CONNECTED) 
+    {
+      delay(500);          
+      display.print(">");   
+      display.display();                 
+    }
+
+    Serial.println(" print_count " + String(print_count));
+    
+    if ((eep && !print_count) || (!eep && print_count))
+    {
+      Serial.println("WiFi Connected.");
+      display.println(char(1));
+      display.display();
+      Serial.print("IP Address: ");
+      Serial.println(WiFi.localIP());
+      display.println(WiFi.localIP());
+      display.display();
+
+      print_count++;
+    } 
+
+    if (WiFi.status() == WL_CONNECTED && eep == false) {update_eep();}
+  } 
    
   if (WiFi.status() == WL_CONNECTED && !start) 
   {
@@ -311,17 +328,28 @@ void read_eep()
   // read eeprom for ssid and pass
   Serial.println("Reading EEPROM ssid");
   String esid;
+  String lttr;
   for (int i = 0; i < 32; ++i)
-    {
-      esid += char(EEPROM.read(i));
+    {  
+      lttr = String(char(EEPROM.read(i)));    
+      if (lttr != "") 
+      {
+        esid += char(EEPROM.read(i));
+        Serial.println("EEPROM SHIT " + String(char(EEPROM.read(i))));
+      }
     }
   Serial.print("SSID: ");
   Serial.println(esid);
   Serial.println("Reading EEPROM pass");
-  String epass = "";
+  String epass; // "";
+  String pass;
   for (int i = 32; i < 96; ++i)
     {
-      epass += char(EEPROM.read(i));
+      pass = String(char(EEPROM.read(i))); 
+      if (pass != "")
+      {
+        epass += char(EEPROM.read(i));
+      }
     }
   Serial.print("PASS: ");
   Serial.println(epass); 
@@ -356,105 +384,5 @@ void update_eep()
       Serial.println(qpass[i]); 
     }    
   EEPROM.commit();
-  Serial.print("saved to eeprom... reset to boot into new wifi");    
-}
-
-/* 4-Way Button:  Click, Double-Click, Press+Hold, and Press+Long-Hold Test Sketch
-
-By Jeff Saltzman
-Oct. 13, 2009
-
-To keep a physical interface as simple as possible, this sketch demonstrates generating four output events from a single push-button.
-1) Click:  rapid press and release
-2) Double-Click:  two clicks in quick succession
-3) Press and Hold:  holding the button down
-4) Long Press and Hold:  holding the button for a long time 
-*/
-
-//=================================================
-//  MULTI-CLICK:  One Button, Multiple Events
-
-// Button timing variables
-int debounce = 20;          // ms debounce period to prevent flickering when pressing or releasing the button
-int DCgap = 250;            // max ms between clicks for a double click event
-int holdTime = 1000;        // ms hold period: how long to wait for press+hold event
-int longHoldTime = 3000;    // ms long hold period: how long to wait for press+hold event
-
-// Button variables
-boolean buttonVal = HIGH;   // value read from button
-boolean buttonLast = HIGH;  // buffered value of the button's previous state
-boolean DCwaiting = false;  // whether we're waiting for a double click (down)
-boolean DConUp = false;     // whether to register a double click on next release, or whether to wait and click
-boolean singleOK = true;    // whether it's OK to do a single click
-long downTime = -1;         // time the button was pressed down
-long upTime = -1;           // time the button was released
-boolean ignoreUp = false;   // whether to ignore the button release because the click+hold was triggered
-boolean waitForUp = false;        // when held, whether to wait for the up event
-boolean holdEventPast = false;    // whether or not the hold event happened already
-boolean longHoldEventPast = false;// whether or not the long hold event happened already
-
-int checkButton() {  
-   int event = 0;  
-   buttonVal = digitalRead(BTTN[0]);
-   // Serial.println("button pressed " + String(buttonVal));
-   // Button pressed down
-   if (buttonVal == LOW && buttonLast == HIGH && (millis() - upTime) > debounce)
-   {
-       downTime = millis();
-       ignoreUp = false;
-       waitForUp = false;
-       singleOK = true;
-       holdEventPast = false;
-       longHoldEventPast = false;
-       if ((millis()-upTime) < DCgap && DConUp == false && DCwaiting == true)  DConUp = true;
-       else  DConUp = false;
-       DCwaiting = false;
-   }
-   // Button released
-   else if (buttonVal == HIGH && buttonLast == LOW && (millis() - downTime) > debounce)
-   {        
-       if (not ignoreUp)
-       {
-           upTime = millis();
-           if (DConUp == false) DCwaiting = true;
-           else
-           {
-               event = 2;
-               DConUp = false;
-               DCwaiting = false;
-               singleOK = false;
-           }
-       }
-   }
-   // Test for normal click event: DCgap expired
-   if ( buttonVal == HIGH && (millis()-upTime) >= DCgap && DCwaiting == true && DConUp == false && singleOK == true && event != 2)
-   {
-       event = 1;
-       DCwaiting = false;
-   }
-   // Test for hold
-   if (buttonVal == LOW && (millis() - downTime) >= holdTime) {
-       // Trigger "normal" hold
-       if (not holdEventPast)
-       {
-           event = 3;
-           waitForUp = true;
-           ignoreUp = true;
-           DConUp = false;
-           DCwaiting = false;
-           //downTime = millis();
-           holdEventPast = true;
-       }
-       // Trigger "long" hold
-       if ((millis() - downTime) >= longHoldTime)
-       {
-           if (not longHoldEventPast)
-           {
-               event = 4;
-               longHoldEventPast = true;
-           }
-       }
-   }
-   buttonLast = buttonVal;
-   return event;
+  Serial.print("saved to eeprom...");    
 }
